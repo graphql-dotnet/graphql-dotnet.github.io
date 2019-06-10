1ad701fbf3adc36d33dbdb30a71a8e48e9a0fb86
Joe McBride <joe@uicraftsman.com>
2019-06-10 10:28:57 -0700

    Revert "Documentation update for 3.0.0"
    This reverts commit 29023a4ebd33569621bb57056951253fb576044d.

---
+++ component---src-templates-docs-page-js-fc9f7d8d738a0f1f08d6.js
@@ -0,0 +1,2 @@
+webpackJsonp([28613242278168],{130:function(e,t,a){"use strict";function r(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0,t.query=void 0;var n=a(2),u=r(n),l=a(3),d=r(l),o=function(e){var t=e.data,a=t.markdownRemark;return u.default.createElement("article",{className:"content",dangerouslySetInnerHTML:{__html:a.html}})};o.propTypes={data:d.default.object};t.query="** extracted graphql fragment **";t.default=o}});
+//# sourceMappingURL=component---src-templates-docs-page-js-fc9f7d8d738a0f1f08d6.js.map
